6ee3303f32814272ff56d28ead893b6198831932
Peter McConnell <pmcconnell@statestreet.com>
2018-07-19 12:55:05 +0100

    tidy ups

--- requirements.sh
+++ requirements.sh
@@ -1,16 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # os-specific
 if [[ "$OSTYPE" == "darwin"* ]]; then
   ./requirements/osx.sh
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
   ./requirements/linux-gnu.sh
-fi
-
-# general (requirements installed in os-specific area)
-if ! [ -x "$(command -v wtf)" ]; then
-  go get github.com/senorprogrammer/wtf
-  cd $GOPATH/src/github.com/senorprogrammer/wtf
-  make install
-  make run
 fi
