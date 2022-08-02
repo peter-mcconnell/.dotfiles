2f8821d8cbec425821f323633f15a77a4e4abad7
Peter McConnell <me@petermcconnell.com>
2022-08-02 15:49:01 +0100

    disabling vims highligher

--- config/nvim/lua/peter-mcconnell/treesitter.lua
+++ config/nvim/lua/peter-mcconnell/treesitter.lua
@@ -6,6 +6,6 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
     disable = { "" },
-    additional_vim_regex_highlighting = true,
+    additional_vim_regex_highlighting = false,
   },
 }
