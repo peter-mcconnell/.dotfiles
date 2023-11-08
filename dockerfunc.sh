02c8314748e3aad1935fb381a2f2f8902a463461
Peter McConnell <me@petermcconnell.com>
2023-11-08 09:31:44 +0000

    adding jaeger

--- dockerfunc.sh
+++ dockerfunc.sh
@@ -682,3 +682,20 @@ d_redpanda() {
     --pandaproxy-addr "PLAINTEXT://0.0.0.0:28082,OUTSIDE://0.0.0.0:8082" \
     --advertise-pandaproxy-addr "PLAINTEXT://${LOCALIP}:28082,OUTSIDE://localhost:8082"
 }
+
+d_jaeger() {
+  docker run -d --name jaeger \
+  -e "COLLECTOR_ZIPKIN_HOST_PORT=${COLLECTOR_ZIPKIN_HOST_PORT:-:9411}" \
+  -e "COLLECTOR_OTLP_ENABLED=${COLLECTOR_OTLP_ENABLED:-true}" \
+  -p 6831:6831/udp \
+  -p 6832:6832/udp \
+  -p 5778:5778 \
+  -p 16686:16686 \
+  -p 4317:4317 \
+  -p 4318:4318 \
+  -p 14250:14250 \
+  -p 14268:14268 \
+  -p 14269:14269 \
+  -p 9411:9411 \
+  jaegertracing/all-in-one:latest
+}
