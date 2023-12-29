56a4c6291c7b468d217bce536747e6181da9aa48
Peter McConnell <me@petermcconnell.com>
2023-12-29 13:44:50 +0000

    chore: removing theme background

--- config/nvim/lua/peter-mcconnell/rice.lua
+++ config/nvim/lua/peter-mcconnell/rice.lua
@@ -43,4 +43,20 @@ require('lualine').setup {
 }
 
 -- set theme colorscheme
+require("catppuccin").setup({
+  flavour = "mocha",
+  transparent_background = true,
+  integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
 vim.cmd.colorscheme "catppuccin"
