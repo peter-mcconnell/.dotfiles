5c5e2cf8b151e3246a1d934b85986d2cb086e338
Peter McConnell <peter.mcconnell@upwind.io>
2024-12-06 11:47:51 +0000

    gcloud + nvm

--- bash_profile.sh
+++ bash_profile.sh
@@ -51,3 +51,17 @@ test -f "$HOME/.cargo/env" && . "$HOME/.cargo/env"
 test -d "$HOME/.krew/bin" && export PATH="$PATH:$HOME/.krew/bin"
 
 true
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/petermcconnell/google-cloud-sdk/path.bash.inc' ]; then . '/Users/petermcconnell/google-cloud-sdk/path.bash.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/petermcconnell/google-cloud-sdk/completion.bash.inc' ]; then . '/Users/petermcconnell/google-cloud-sdk/completion.bash.inc'; fi
+
+if [ -f ~/.git-completion.bash ]; then
+  . ~/.git-completion.bash
+fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
